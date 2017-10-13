1088595ef7e73f45e5919f014efc3f8d72374390
4lex4 <4lex49@zoho.com>
2017-10-13 18:12:24 +0300

    ~ skipping qtquick qtlocation

--- cmake/generate_qt_build_script.cmake
+++ cmake/generate_qt_build_script.cmake
@@ -3,6 +3,7 @@ FILE(
         "configure -platform ${PLATFORM} ${BUILD_TYPE} -shared"
 		" -opengl desktop"
         " -system-zlib -system-libpng -system-libjpeg"
+		" -skip qtquick1 -skip qtlocation"
         " -nomake examples -nomake tests"
         " -opensource -confirm-license -no-ltcg"
         " -I \"${JPEG_INCLUDE_DIR}\" -I \"${ZLIB_INCLUDE_DIR}\""
