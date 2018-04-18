24f312eaf2f301285b6487579d320b2b27e91c48
Alex <4lex49@zoho.com>
2018-04-19 02:22:49 +0300

    ~ CMake code reformat and fixes.

--- cmake/generate_boost_build_script.cmake
+++ cmake/generate_boost_build_script.cmake
@@ -1,4 +1,4 @@
-FILE(
+file(
         WRITE "${TARGET_FILE}"
         "bootstrap.bat ${TOOLSET}\n"
         "b2 -q --with-test toolset=${TOOLSET} link=static threading=multi ${EXTRA_PARAMS} -j $ENV{NUMBER_OF_PROCESSORS} stage\n"
