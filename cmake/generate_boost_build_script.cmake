7fa97a8c8bd516133792086ac5af62f0fa952e65
Alex <4lex49@zoho.com>
2018-06-15 04:16:54 +0300

    ~ Reformat all the code.
    2 spaces indentation for CMake files.

--- cmake/generate_boost_build_script.cmake
+++ cmake/generate_boost_build_script.cmake
@@ -1,8 +1,8 @@
 file(
-        WRITE "${TARGET_FILE}"
-        "call "
-        "bootstrap.bat ${TOOLSET}\n"
-        "if %ERRORLEVEL% EQU 0 call "
-        "b2 -q --with-test toolset=${TOOLSET} address-model=${ADDRESS_MODEL} variant=${BUILD_TYPE}"
-        " link=static runtime-link=static threading=multi ${EXTRA_PARAMS} -j $ENV{NUMBER_OF_PROCESSORS} stage\n"
+    WRITE "${TARGET_FILE}"
+    "call "
+    "bootstrap.bat ${TOOLSET}\n"
+    "if %ERRORLEVEL% EQU 0 call "
+    "b2 -q --with-test toolset=${TOOLSET} address-model=${ADDRESS_MODEL} variant=${BUILD_TYPE}"
+    " link=static runtime-link=static threading=multi ${EXTRA_PARAMS} -j $ENV{NUMBER_OF_PROCESSORS} stage\n"
 )
