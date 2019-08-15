947bf2e595a68be40b823cd675eca52ec37d9c24
4lex4 <4lex49@zoho.com>
2019-08-15 11:55:47 +0300

    ~ Add a check whether the toolchain has already been installed

--- configure_msys.bat
+++ configure_msys.bat
@@ -4,7 +4,7 @@ chcp 437 >nul
 
 :: checking for administrative permissions
 net session >nul 2>nul
-IF !ERRORLEVEL! NEQ 0 (
+IF !ERRORLEVEL! neq 0 (
 	echo ERROR: Administrative permissions required. Exiting... >&2
 	echo. & pause
 	exit /b 2
@@ -26,20 +26,37 @@ if not exist "!msys2_dir!" (
 
 if exist "%MINGW32_PATH%" (
 	set link=!msys2_dir!\mingw32
-	rd /s /q "!link!" >nul 2>nul
-	mklink /d !link! %MINGW32_PATH% >nul
-	if !ERRORLEVEL! equ 0 (
-		echo INFO: mingw32 is ready to use with msys2.
-	)
+  call :folder_is_empty "!link!\bin"
+  if !ERRORLEVEL! equ 1 (
+    rd /s /q "!link!" >nul 2>nul
+    mklink /d !link! %MINGW32_PATH% >nul
+    if !ERRORLEVEL! equ 0 (
+      echo INFO: mingw32 is ready to use with msys2.
+    )
+  ) else (
+    echo INFO: mingw32 has already been installed.
+  )
 )
 
 if exist "%MINGW64_PATH%" (
 	set link=!msys2_dir!\mingw64
-	rd /s /q "!link!" >nul 2>nul
-	mklink /d !link! %MINGW64_PATH% >nul
-	if !ERRORLEVEL! equ 0 (
-		echo INFO: mingw64 is ready to use with msys2.
-	)
+  call :folder_is_empty "!link!\bin"
+  if !ERRORLEVEL! equ 1 (
+    rd /s /q "!link!" >nul 2>nul
+    mklink /d !link! %MINGW64_PATH% >nul
+    if !ERRORLEVEL! equ 0 (
+      echo INFO: mingw64 is ready to use with msys2.
+    )
+  ) else (
+    echo INFO: mingw64 has already been installed.
+  )
 )
 
 echo. & pause
+exit /b %ERRORLEVEL%
+
+:folder_is_empty
+for /F %%a in ('dir /b "%~1" 2^>nul') do (
+  exit /b 0
+)
+exit /b 1
