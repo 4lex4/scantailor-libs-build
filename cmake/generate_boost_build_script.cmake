3be456a3aa40ddcdcb4d01f08ac34d74eed1df21
4lex4 <4lex49@zoho.com>
2020-04-02 19:51:22 +0300

    Build qt and boost in the separate directory

--- cmake/generate_boost_build_script.cmake
+++ cmake/generate_boost_build_script.cmake
@@ -3,8 +3,9 @@ include(${SOURCE_DIR}/cmake/ToNativePath.cmake)
 file_to_native_path("${INSTALL_DIR}" INSTALL_DIR)
 
 file(WRITE "${TARGET_FILE}"
+    "mkdir build\n"
     "call bootstrap.bat ${TOOLSET}\n"
     "if %ERRORLEVEL% equ 0 call "
-    "b2 -q --with-test toolset=${TOOLSET} address-model=${ADDRESS_MODEL} variant=${BUILD_TYPE}"
+    "b2 --build-dir=build -q --with-test toolset=${TOOLSET} address-model=${ADDRESS_MODEL} variant=${BUILD_TYPE}"
     " link=static runtime-link=shared threading=multi ${EXTRA_PARAMS} -j $ENV{NUMBER_OF_PROCESSORS}"
     " --prefix=\"${INSTALL_DIR}\" install\n")
