d727643b11871c2f66ad3a74def796b27c52cc98
4lex4 <4lex49@zoho.com>
2020-04-02 20:04:24 +0300

    Disable tiff unused modules on configuring with mingw
    In case there are devel packages of lzma, webp or zstd installed in msys2 environment by default tiff is configured with those modules enabled but we don't build and use those dependencies so the error occurred on building tiff.

--- configure_libs.bat
+++ configure_libs.bat
@@ -17,37 +17,49 @@ set png_dir_name_2=lpng[0-9]*
 set tiff_dir_name=tiff-[0-9]*.[0-9]*.[0-9]*
 
 for /f "delims=" %%a in ('dir "%PATH_TO_TOOLS%" /b /Ad ^| findstr /r /C:"%MSYS2_DIR_NAME%"') do set msys2_dir=%PATH_TO_TOOLS%%%a
-
 if not exist "!msys2_dir!" (
 	echo ERROR: msys2 not found. Exiting... >&2
 	echo. & pause
 	exit /b 1
 )
+set MSYS2_SHELL="!msys2_dir!\msys2_shell.cmd"
 
 for /f "delims=" %%a in ('dir "%PATH_TO_LIBS%" /b /Ad ^| findstr /r /C:"%jpeg_dir_name%"') do set jpeg_dir=%PATH_TO_LIBS%\%%a
-
-if exist "!jpeg_dir!" (
-	call !msys2_dir!\msys2_shell.cmd -%MINGW% -where "!jpeg_dir!" -c "./configure"
-) else (
-	echo ERROR: jpeg_dir not found. >&2
-	pause
-)
-
 for /f "delims=" %%a in ('dir "%PATH_TO_LIBS%" /b /Ad ^| findstr /r /C:"%png_dir_name_1%"') do set png_dir=%PATH_TO_LIBS%\%%a
 for /f "delims=" %%a in ('dir "%PATH_TO_LIBS%" /b /Ad ^| findstr /r /C:"%png_dir_name_2%"') do set png_dir=%PATH_TO_LIBS%\%%a
-
-if exist "!png_dir!" (
-	call !msys2_dir!\msys2_shell.cmd -%MINGW% -where "!png_dir!" -c "./configure"
-) else (
-	echo ERROR: png_dir not found. >&2
-	pause
-)
-
 for /f "delims=" %%a in ('dir "%PATH_TO_LIBS%" /b /Ad ^| findstr /r /C:"%tiff_dir_name%"') do set tiff_dir=%PATH_TO_LIBS%\%%a
 
-if exist "!tiff_dir!" (
-	call !msys2_dir!\msys2_shell.cmd -%MINGW% -where "!tiff_dir!" -c "./configure"
+call :configure "jpeg" "!jpeg_dir!"
+call :configure "png" "!png_dir!"
+call :configure "tiff" "!tiff_dir!" "--disable-zstd --disable-lzma --disable-webp --disable-jbig"
+
+exit /b %ERRORLEVEL%
+
+
+::::::::::::::::::::::::::::::::::::::::
+:: Calls 'configure' script for a given path in msys2 environment.
+:: Globals:
+::   MINGW  MinGW-w64 toolchain version
+::   MSYS2_SHELL  The path to msys2_shell.cmd
+:: Arguments:
+::   Name of the target configured
+::   The directory with the 'configure' script.
+::   Arguments for the configure script
+:: Return:
+::   None
+::::::::::::::::::::::::::::::::::::::::
+:configure
+setlocal
+set name=%~1
+set dir=%~2
+set arguments=%~3
+
+if exist "%dir%" (
+	call %MSYS2_SHELL% -%MINGW% -where "%dir%" -c "./configure %arguments%"
 ) else (
-	echo ERROR: tiff_dir not found. >&2
+	echo ERROR: %name% not found. >&2
 	pause
 )
+
+endlocal
+exit /b %ERRORLEVEL%
--- configure_msys.bat
+++ configure_msys.bat
@@ -16,46 +16,50 @@ set MINGW64_PATH=%PATH_TO_TOOLS%\mingw64
 set MSYS2_DIR_NAME=msys*
 
 for /f "delims=" %%a in ('dir "%PATH_TO_TOOLS%" /b /Ad ^| findstr /r /C:"%MSYS2_DIR_NAME%"') do set msys2_dir=%PATH_TO_TOOLS%%%a
-
 if not exist "!msys2_dir!" (
 	echo ERROR: msys2 not found. Exiting... >&2
 	echo. & pause
 	exit /b 1
 )
 
-if exist "%MINGW32_PATH%" (
-	set link=!msys2_dir!\mingw32
-  call :folder_is_empty "!link!\bin"
-  if !ERRORLEVEL! equ 1 (
-    rd /s /q "!link!" >nul 2>nul
-    mklink /d !link! %MINGW32_PATH% >nul
-    if !ERRORLEVEL! equ 0 (
-      echo INFO: mingw32 is ready to use with msys2.
-    )
-  ) else (
-    echo INFO: mingw32 has already been installed.
-  )
-)
-
-if exist "%MINGW64_PATH%" (
-	set link=!msys2_dir!\mingw64
-  call :folder_is_empty "!link!\bin"
-  if !ERRORLEVEL! equ 1 (
-    rd /s /q "!link!" >nul 2>nul
-    mklink /d !link! %MINGW64_PATH% >nul
-    if !ERRORLEVEL! equ 0 (
-      echo INFO: mingw64 is ready to use with msys2.
-    )
-  ) else (
-    echo INFO: mingw64 has already been installed.
-  )
-)
+call :configure_msys "!msys2_dir!" "32" "%MINGW32_PATH%"
+call :configure_msys "!msys2_dir!" "64" "%MINGW64_PATH%"
 
 echo. & pause
 exit /b %ERRORLEVEL%
 
+
+:configure_msys
+setlocal
+set msysPath=%~1
+set version=%~2
+set mingwPath=%~3
+if exist "%mingwPath%" (
+	set link=%msysPath%\mingw%version%
+  call :folder_is_empty "!link!\bin"
+  if !ERRORLEVEL! equ 1 (
+    rd /s /q "!link!" >nul 2>nul
+    mklink /d !link! %mingwPath% >nul
+    if !ERRORLEVEL! equ 0 (
+      echo INFO: mingw%version% is ready to use with msys2.
+    )
+  ) else (
+    echo INFO: mingw%version% has already been installed.
+  )
+)
+endlocal
+exit /b %ERRORLEVEL%
+
+::::::::::::::::::::::::::::::::::::::::
+:: Check whether the folder is emtpy.
+:: Arguments:
+::   The directory to check.
+:: Return:
+::   ERRORLEVEL set to 0 if the given
+::   folder not empty and 1 otherwise.
+::::::::::::::::::::::::::::::::::::::::
 :folder_is_empty
-for /F %%a in ('dir /b "%~1" 2^>nul') do (
+for /f %%a in ('dir /b "%~1" 2^>nul') do (
   exit /b 0
 )
 exit /b 1
